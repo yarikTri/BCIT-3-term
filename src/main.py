de3d5dd245afce0db8e88ceb5f94d7779b5c3649
Yaroslav Kuzmin <yarik1448kuzmin@gmail.com>
2022-10-19 23:29:47 +0300

    super-library added

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
 from lab_python_oop.rectangle import Rectangle
 from lab_python_oop.circle import Circle
 from lab_python_oop.square import Square
-import emoji
+import pyjokes
 
 def main():
-    r = Rectangle("синего", 12, 12)
-    c = Circle("зеленого", 12)
-    s = Square("красного", 12)
+    r = Rectangle("синего", 3, 4)
+    c = Circle("зеленого", 8)
+    s = Square("красного", 10)
     print(r)
     print(c)
     print(s)
 
-    result = emoji.emojize('Python is :thumbs_up:')
-    print(result)
+    print(pyjokes.get_joke())
 
 if __name__ == "__main__":
     main()
